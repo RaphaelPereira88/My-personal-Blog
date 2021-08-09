d72cc1e0776cd178fd9de8c980849a8285bc2565
Raphael Pereira <raphael.pereira88@gmail.com>
2021-08-09 19:18:21 +0100

    Update app.js

--- blogWebsite/js/app.js
+++ blogWebsite/js/app.js
@@ -3,32 +3,28 @@
  * 
 */
 const navBarItems =[];
-const sections = document.querySelectorAll('section');
-const nav = document.getElementById('nav__bar2');
-const mobileMenu = document.getElementById('menu');
+const sections = $('section');
+const nav = $('#nav__bar2');
+const mobileMenu = $('#menu');
 
 /*
  * Functions to create Nav
  
 */
-
 const create_nav = (sections)=> {
     for (let section of sections){
-        const liElement = document.createElement('li');
-        const aElement = document.createElement('a');
-        aElement.innerHTML = document.getElementsByClassName(section.className)[0].id;
-        aElement.setAttribute('class', aElement.innerHTML);
-        aElement.style.setProperty('cursor', 'pointer');
-        liElement.appendChild(aElement);
-        nav.appendChild(liElement);
-/*need to create an object to be able to export those datas in the active function  below !!*/
+        let li = $('<li><a></a></li>');
+        let link = li.children('a')
+        link.text(section.className);
+        nav.append(li);
+/*need to create an array to be able to export those datas in the Add clactive below !!*/
         const navBarItem = {
-            element: aElement,
+            element: link,
             Section: section
         };
         navBarItems.push(navBarItem);
 /* Scroll to section on link click*/
-        liElement.addEventListener('click',()=> {
+        li.on('click',()=> {
             event.preventDefault();
             const top = section.getBoundingClientRect().top + window.pageYOffset;
             window.scroll({top, behavior: 'smooth'});
@@ -36,10 +32,8 @@ const create_nav = (sections)=> {
     };
 };
 
-
 /* build the nav*/
 create_nav(sections);
-
 
 /* Add class 'active' to section when near top of viewport
 use the array to be able to give the class active to the right <a> and use css 
@@ -49,18 +43,20 @@ const active = ()=>{
     for ( let {element ,Section} of navBarItems){
         const position = Section.getBoundingClientRect();
         if (position.top<= 400 && position.bottom >= 500){
-            element.classList= 'activated';
+            element.addClass ('activated');
         }else{
-            element.classList= '';
+            element.removeClass('activated')
         };
     };
 };
 
+
 /* Scroll to anchor ID using scrollTO event*/
-window.addEventListener('scroll',active);
+$(window).on('scroll',active);
 
 /* to Create A Mobile Navigation Menu */
 /* Create a toggle event in navigation */
-mobileMenu.addEventListener('click', ()=> {
-    nav.classList.toggle('active');
+mobileMenu.on('click', ()=> {
+    nav.toggleClass('active');
 });
+
